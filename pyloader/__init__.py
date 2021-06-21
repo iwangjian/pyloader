1d743e53aeedd119f36ff65ad5da0b174167268c
iwangjian <1049136551@qq.com>
2021-06-21 21:16:20 +0800

    update

--- pyloader/__init__.py
+++ pyloader/__init__.py
@@ -1,4 +1,36 @@
-from .datareader import DataReader
-from .dataset import Dataset
-from .datacollator import DataCollator
-from .dataloader import DataLoader
+###########################################################
+# METADATA
+###########################################################
+
+# Version.  For each new release, the version number should be updated
+# in the file VERSION.
+import os
+
+try:
+    # If a VERSION file exists
+    version_file = os.path.join(os.path.dirname(__file__), "VERSION")
+    with open(version_file, "r") as infile:
+        __version__ = infile.read().strip()
+except NameError:
+    __version__ = "unknown (running code interactively?)"
+except IOError as ex:
+    __version__ = "unknown (%s)" % ex
+
+if __doc__ is not None:  # fix for the ``python -OO``
+    __doc__ += "\n@version: " + __version__
+
+__license__ = "Apache License, Version 2.0"
+__longdescr__ = "An asynchronous Python dataloader for loading big datasets with limited memory."
+__keywords__ = [
+    "deep learning",
+    "data loader",
+]
+
+###########################################################
+# TOP-LEVEL MODULES
+###########################################################
+
+from pyloader.datareader import *
+from pyloader.dataset import *
+from pyloader.datacollator import *
+from pyloader.dataloader import *
--- pyloader/datacollator.py
+++ pyloader/datacollator.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from abc import ABC, abstractmethod
 from typing import Any, Dict, List
-import torch
+import numpy as np
 
 
 class DataCollator(ABC):
@@ -20,9 +20,9 @@ class DataCollator(ABC):
         pass
 
 
-class PyDataCollator(DataCollator):
+class NumpyDataCollator(DataCollator):
     """
-    Simple PyTorch-based data collator that:
+    Simple data collator using `numpy.array` that:
     - simply collates batches of dict-like objects
     - performs special handling for potential keys named:
         - `label`: handles a single value (int or float) per object
@@ -30,16 +30,16 @@ class PyDataCollator(DataCollator):
     i.e., Property names of the input object will be used as corresponding inputs to the model.
     """
 
-    def collate_batch(self, features: Dict[str, List]) -> Dict[str, torch.Tensor]:
+    def collate_batch(self, features: Dict[str, List]) -> Dict[str, np.array]:
         # Special handling for labels.
         # Ensure that tensor is created with the correct type
         # (it should be automatically the case, but let's make sure of it.)
         if "label" in features and features["label"] is not None:
             first = features["label"][0]
             if type(first) is int:
-                labels = torch.tensor([f for f in features["label"]], dtype=torch.long)
+                labels = np.array([f for f in features["label"]], dtype=np.int)
             else:
-                labels = torch.tensor([f for f in features["label"]], dtype=torch.float)
+                labels = np.array([f for f in features["label"]], dtype=np.float)
             batch = {"label": labels}
         else:
             batch = {}
@@ -48,6 +48,6 @@ class PyDataCollator(DataCollator):
         # Again, we will use the first element to figure out which key/values are not None for this model.
         for k, v in features.items():
             if k not in ("label") and v is not None:
-                batch[k] = torch.tensor([f for f in features[k]], dtype=torch.long)
+                batch[k] = np.array([f for f in features[k]], dtype=np.float)
         return batch
         
